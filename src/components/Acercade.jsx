28b9c3da7f6bbe48d132517a81dfabe0cbe35b04
y123an <yohannesaregahabte@gmail.com>
2024-03-16 12:42:20 +0300

    updates

--- src/components/Acercade.jsx
+++ src/components/Acercade.jsx
@@ -4,48 +4,41 @@ import MicBox from "../assets/Acercade/mic_box.png";
 
 const Acercade = () => {
   return (
-    <div className="flex flex-col w-full p-8 md:p-36 gap-8 md:gap-20">
-      <div className="w-full flex">
-        <h1 className="text-4xl">
-          <span className="uppercase">
-            Acer<span className="text-gray-200">ca</span>
-          </span>{" "}
-          de
-        </h1>
-      </div>
-      <div className="flex flex-col gap-5 md:gap-10">
-        <p className="w-full md:w-[50%] text-lg font-semibold">
-          Históricamente se ha comprobado que los hongos mágicos (Psilocibe) son
-          responsables por varios avances mentales de nuestra especie. Aho
-          científicamente se está comprobando la efectividad de esta molécula en
-          curar a pacientes con drogadicción, depresión, ansiedad y
-          anti-sociabilidad, entre otras. ¡Perciba cambios positivos desde la
-          primer toma!
-        </p>
-        <div className="flex flex-col gap-5 font-semibold">
-          <DownloadItem title="PREGUNTAS FRECUENTES Y RESPUESTAS CON CITAS (PDF)" />
-          <DownloadItem title="PROTOCOLO DE MICRODÓSIS DE PAUL STAMETS (PDF)" />
-          <DownloadItem title="IMÁGENES ILUSTRATIVAS PARA INSTAGRAM (JPG)" />
-          <DownloadItem title="VIDEO DEMOSTRANDO EL PRODUCTO Y SUS USOS (MP4)" />
+    <div className="flex justify-center gap-20 p-10">
+      <div className="flex justify-center  flex-col w-[600px] gap-10 md:gap-10">
+        <div className="flex justify-center">
+          <h1 className="text-[50px] border-2 border-black p-1">
+            <span className="uppercase">
+              Acer<span className="text-gray-200">ca</span>
+            </span>{" "}
+            de
+          </h1>
         </div>
-        <img
-          src={MicBox}
-          alt=""
-          className="hidden md:block absolute top-[900px] left-[900px] -z-10"
-        />
-      </div>
-      <div className="flex flex-col md:flex-row md:gap-20">
-        <div className="flex">
-          <p className="bg-black py-2 px-5 text-white">
-            REPRODUZCA EL VIDEO E INFORMESE
-          </p>
-        </div>
-        <p className="w-full md:w-[50%]">
-          <span className="text-yellow-400">Dato curioso:</span> Fué vuelta
-          ilegal en 1973 precisamente porque libera la mente de sistemas de
-          pensar que atan.
+        <p className="text-lg font-semibold">
+          Históricamente se ha comprobado que los hongos mágicos (Psilocibe
+          Cubensis) son responsables por varios avances mentales de nuestra
+          especie. Ahora, científicamente se está comprobando la efectividad de
+          esta molécula en curar a pacientes con drogadicción, depresión,
+          ansiedad y anti-sociabilidad, entre otras. ¡Sane el pasado y retome su
+          esencia!
         </p>
+        <div className="gap-5 grid">
+          <div className="flex flex-col gap-5 font-semibold">
+            <DownloadItem title="PREGUNTAS FRECUENTES Y RESPUESTAS CON CITAS (PDF)" />
+            <DownloadItem title="PROTOCOLO DE MICRODÓSIS DE PAUL STAMETS (PDF)" />
+            <DownloadItem title="IMÁGENES ILUSTRATIVAS PARA INSTAGRAM (JPG)" />
+            <DownloadItem title="VIDEO DEMOSTRANDO EL PRODUCTO Y SUS USOS (MP4)" />
+          </div>
+          <div className="">
+            <div className=" text-center">
+              <p className="bg-black py-2 px-5 text-white">
+                REPRODUCE EL VIDEO E INFÓRMATE
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+      <img src={MicBox} alt="" className="hidden md:block w-[300px]" />
     </div>
   );
 };
